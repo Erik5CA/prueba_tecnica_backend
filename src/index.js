09d490f5e493d630eb26ca99a3edd88cff7687c1
Erik5CA <erik.castillo.anzures@gmail.com>
2024-12-24 19:28:23 -0600

    establishes connection to the database and starts the application

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import app from "./app.js";
+import { sequelize } from "./database/database.js";
+import "./models/user.model.js";
+
+async function main() {
+  try {
+    await sequelize.sync();
+    console.log("Connection has been established successfully.");
+    app.listen(3000, () => {
+      console.log("Server started on port 3000");
+    });
+  } catch (error) {
+    console.error("Unable to connect to the database:", error);
+  }
+}
+
+main();
